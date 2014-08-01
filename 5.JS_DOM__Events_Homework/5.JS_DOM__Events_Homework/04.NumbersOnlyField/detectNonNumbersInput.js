9b740c5957361ef9c5ef5389e6c6d53ec9b7b75c
Venelin <ipconfigv@gmail.com>
2014-08-01 11:42:52 +0300

    edit 4.Numbers Only Field

--- 5.JS_DOM__Events_Homework/5.JS_DOM__Events_Homework/04.NumbersOnlyField/detectNonNumbersInput.js
+++ 5.JS_DOM__Events_Homework/5.JS_DOM__Events_Homework/04.NumbersOnlyField/detectNonNumbersInput.js
@@ -1,18 +1,19 @@
-﻿/*4.Write a HTML page holding a form and a text field. Using JavaScript make the 
-text field to accept numbers only. When a non-number character is entered through 
-the keyboard (or by any other way), make the field red for a while and do not accept 
-the change (preserve the previous value of the field).*/
+﻿function detectInput() {
+    var input = document.getElementById("insert");
+    var inputVal = input.value;
+   
+    if (!isFinite(inputVal)) {
+        var regex = /\D/g;
+        input.value = inputVal.replace(regex, '');
 
-function detectInput() {
-    var name = document.getElementById("insert");
-    var value = name.value;
-
-    if (!isFinite(value)) {
         document.getElementById('detect').style.backgroundColor = 'red';
+        document.getElementById('insert').style.backgroundColor = 'red';
         var sound = new Audio("sound/button-18.wav"); // buffers automatically when created
         sound.play();
     } else {
-        document.getElementById('detect').value = value;
+        document.getElementById('detect').value = inputVal;
+        document.getElementById('insert').style.backgroundColor = '#00ff21';
+        document.getElementById('detect').style.backgroundColor = '#00ff21';
     }
 }
 
